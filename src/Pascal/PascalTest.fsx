3032f5bbe2cef8182e3ad39845af85c3076390b1
MikkelVogelius <mikkelvogelius@gmail.com>
2025-10-20 14:59:28 +0200

    added property2 test

--- src/Pascal/PascalTest.fsx
+++ src/Pascal/PascalTest.fsx
@@ -2,7 +2,7 @@
 open Pascal
 
 let hasPropertyOneTest f (n: int, _k: int) : bool = 
-    f (n,0) = 1 && f (n, n) = 1 
+    f (n, 0) = 1 && f (n, n) = 1 
 
 //hasPropertyOne: int * int -> bool
 let hasPropertyOne (n: int, k: int) : bool = 
@@ -13,10 +13,16 @@ let hasPropertyOne (n: int, k: int) : bool =
 let hasPropertyOneNoRec (n: int, k: int): bool = 
     hasPropertyOneTest pascalNoRec (n,k)
 
+
+let hasPropertyTwoTest f (n: int, k: int) : bool =
+    f (n, k) = f (n - 1, k - 1) + f (n - 1, k) 
+
 //hasPropertyTwo: int * int -> bool
-let hasPropertyTwo (n , k) : bool =
-    false
+let hasPropertyTwo (n, k) : bool =
+    hasPropertyTwoTest pascalNoRec (n,k)
 
 //hasPropertyTwoNoRec: int * int -> bool
 let hasPropertyTwoNoRec ( n , k) : bool =
-    false
+    hasPropertyTwoTest pascalNoRec (n,k)
+
+

bc4704db1a0c9dc8a186896f72bcb1b70dcae3c9
sebberas <sebberas@gmail.com>
2025-10-20 20:34:28 +0200

    Added my implementation of MyList

--- src/MyList/MyList.fs
+++ src/MyList/MyList.fs
@@ -5,20 +5,34 @@ type 'a MyList =
     | Something of 'a * 'a MyList
 
 
-//Takes an integer n and a MyList, and returns a MyList of the first n elements in the original list.
-let rec take (n: int) (lst: 'a MyList) : 'a MyList = 
-    Empty
+// Takes an integer n and a MyList, and returns a MyList of the first n elements in the original list.
+let rec take (n: int) (list: 'a MyList): 'a MyList = 
+    match n, list with
+    | 0, _ -> Empty
+    | _, Empty -> Empty
+    | 1, Something (head, _tail) ->  Something (head, Empty) 
+    | _, Something (head, tail) -> 
+        match take (n - 1) tail with
+        | Empty -> Something (head, Empty)
+        | Something (headhead, tail) -> Something (head, Something (headhead, tail))
 
 
-//Takes an integer n and a MyList, and returns a MyList without the first n elements of the original list.
-let rec drop (n: int) (lst : 'a MyList) : 'a MyList =
-    Empty
 
-//Takes a MyList and returns the number of elements in the list.
-let rec length (lst: 'a MyList) : int = 
-    0
+// Takes an integer n and a MyList, and returns a MyList without the first n elements of the original list.
+let rec drop (n: int) (list : 'a MyList) : 'a MyList =
+    match n, list with
+    | 0, _ -> list
+    | _, Empty -> Empty
+    | _, Something (_head, tail) -> drop (n - 1) tail  
 
+// Takes a MyList and returns the number of elements in the list.
+let rec length (list: 'a MyList): int = 
+    match list with
+    | Empty -> 0
+    | Something (_head, tail) -> 1 + length tail 
 
-//Takes a function, and a MyList and returns a MyList with the function applied to each element.
-let rec map (f: 'a -> 'b) (lst: 'a MyList) : 'b MyList = 
-    Empty
+// Takes a function, and a MyList and returns a MyList with the function applied to each element.
+let rec map (f: 'a -> 'b) (list: 'a MyList) : 'b MyList = 
+    match list with
+    | Empty -> Empty
+    | Something (head, tail) -> Something (f head, map f tail)
--- src/MyList/MyListExamples.fsx
+++ src/MyList/MyListExamples.fsx
@@ -1,1 +1,10 @@
-printfn "Hello World"
+#load "MyList.fs"
+open MyList
+
+let list = Something (1, Something (2, Something (3, Something (4, Something (5, Something(6, Empty))))))
+
+printfn "Length: %A" (length list)
+printfn "Take: %A" (take 4 list)
+printfn "Drop: %A" (drop 4 list)
+printfn "Map: %A" (map (fun item -> string (item * 2)) list)
+

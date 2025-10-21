8eba506ca19f094e387ac7025b0be439888270ad
SimonSvarre <simonegebergsvarre@gmail.com>
2025-10-21 11:36:15 +0200

    test

--- src/Pascal/Pascal.fs
+++ src/Pascal/Pascal.fs
@@ -3,7 +3,7 @@ module Pascal
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
 // pascal: int * int -> int
 let pascal (n: int, k: int) : int = 
-    0
+    1
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
 // pascalNoRec: int * int -> int

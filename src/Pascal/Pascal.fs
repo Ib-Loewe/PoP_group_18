fe3d0f4811d8690843d73da3bd730e734a6b6be9
SimonSvarre <simonegebergsvarre@gmail.com>
2025-10-22 11:47:36 +0200

    Added handling of invalid interger input to pascal and pascalNoRec.

--- src/Pascal/Pascal.fs
+++ src/Pascal/Pascal.fs
@@ -2,21 +2,25 @@ module Pascal
 
 let rec pascal (n: int, k: int) : int = 
     match (n,k) with
-    | (a, b) when b = 0 || a = b -> 1
-    | _ -> pascal (n - 1, k - 1) + pascal (n - 1, k)
+    | (n, k) when n < 0 || k < 0 || k > n -> 0
+    | (n, k) when k = 0 || n = k -> 1
+    | (n, k) -> pascal (n - 1, k - 1) + pascal (n - 1, k)
 
 let pascalNoRec (n: int, k: int) : int =
-    // Initialize matrix to size of pascal triangle with zeros 
-    let biMatrix: int array array = Array.zeroCreate (n + 1)
-    for i = 0 to n do
-        biMatrix[i] <- Array.zeroCreate (i + 1)
-    // Fill matrix with binomial coefficients
-    for row = 0 to n do
-        for entry = 0 to row do
-            if entry = 0 || entry = row then
-                biMatrix[row][entry] <- 1
-            else
-                biMatrix[row][entry] <- biMatrix[row - 1][entry - 1] + biMatrix[row - 1][entry] 
-    // Return corresponding binomial coefficient
-    biMatrix[n][k]
+    if n < 0 || k < 0 || k > n then
+        0
+    else
+        // Initialize matrix to size of pascal triangle with zeros 
+        let biMatrix: int array array = Array.zeroCreate (n + 1)
+        for i = 0 to n do
+            biMatrix[i] <- Array.zeroCreate (i + 1)
+        // Fill matrix with binomial coefficients
+        for row = 0 to n do
+            for entry = 0 to row do
+                if entry = 0 || entry = row then
+                    biMatrix[row][entry] <- 1
+                else
+                    biMatrix[row][entry] <- biMatrix[row - 1][entry - 1] + biMatrix[row - 1][entry] 
+        // Return corresponding binomial coefficient
+        biMatrix[n][k]
 
--- src/Pascal/Pascal.fsi
+++ src/Pascal/Pascal.fsi
@@ -1,10 +1,12 @@
 module Pascal
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
+// Returns 0 if n < 0, k < 0 or k > n
 // pascal: int * int -> int
 val pascal: (int * int) -> int;
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
+// Returns 0 if n < 0, k < 0 or k > n
 // pascalNoRec: int * int -> int
 val pascalNoRec: (int * int) -> int
 

aa68c72bc0f5b8047291328ef3cb009ea727703a
sebberas <sebberas@gmail.com>
2025-10-23 10:32:51 +0200

    my beautiful implementation

--- src/Pascal/Pascal.fs
+++ src/Pascal/Pascal.fs
@@ -1,12 +1,37 @@
 module Pascal
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
-// pascal: int * int -> int
-let pascal (n: int, k: int) : int = 
-    0
+let rec pascal (n: int, k: int): int = 
+    match k with
+    | 0 -> 1
+    | k when k = n -> 1
+    | _ -> pascal (n - 1, k - 1) + pascal (n - 1, k)
 
 // Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
-// pascalNoRec: int * int -> int
-let pascalNoRec (n: int, k: int) : int =
-    0
+let pascalNoRec (n: int, k: int): int =
+    let mutable rows: list<array<int>> = [];
 
+    // Early return if n = k?
+
+    // Only compute until k rows!?
+
+    for i in 0..n do
+        match i with
+        | 0 -> rows <- [|1|] :: rows
+        | 1 -> rows <- [|1; 1|] :: rows
+        | _ -> 
+            let column = Array.zeroCreate (i + 1)
+            Array.set column 0 1
+            Array.set column (column.Length - 1) 1
+
+            for j in 1..(i - 1) do
+                Array.set column j (rows.Head[j - 1] + rows.Head[j])      
+            
+            // printfn "%A" column
+            rows <- column :: rows
+
+        // if rows.Length % 100 = 0 then
+        //     printfn "rows: %A" rows.Length
+    
+    printfn "%A" rows[0]
+    rows[0][k]
--- src/Pascal/PascalTest.fsx
+++ src/Pascal/PascalTest.fsx
@@ -23,8 +23,8 @@ let hasPropertyTwoNoRec ( n , k) : bool =
 
 
 // Following is the property tests for the recursive and iterative implementation
-let n = 1_078_456_392 // a random integer in the middle of the input domain ± 10 million
-let max = 2_147_483_647
+let n = 20 // 1_078_456_392 // a random integer in the middle of the input domain ± 10 million
+let max = System.Int32.MaxValue - 1
 
 printfn "Recursive Property 1:"
 printfn "- (0,0): %A" (hasPropertyOne (0, 0))
@@ -38,18 +38,21 @@ printfn "Recursive Property 2:"
 printfn "- (2,1): %A" (hasPropertyTwo (2,1))
 printfn "- (n,n-1): %A" (hasPropertyTwo (n,n-1))
 printfn "- (n,n/2): %A" (hasPropertyTwo (n,n/2))
-printfn "- (max-1,max-2): %A" (hasPropertyTwo (max - 1, max-2))
-printfn "- (max-1,n): %A" (hasPropertyTwo (max-1,n))
-printfn "- (max-1,1): %A" (hasPropertyTwo (max-1,1))
-printfn "- (max,max-1): %A" (hasPropertyTwo (max, max-1))
-printfn "- (max,n): %A" (hasPropertyTwo (max,n))
-printfn "- (max,1): %A" (hasPropertyTwo (max,1)) 
+// printfn "- (2,1): %A" (hasPropertyTwo (2,1))
+// printfn "- (n,n-1): %A" (hasPropertyTwo (n,n-1))
+// printfn "- (n,n/2): %A" (hasPropertyTwo (n,n/2))
+// printfn "- (max-1,max-2): %A" (hasPropertyTwo (max - 1, max-2))
+// printfn "- (max-1,n): %A" (hasPropertyTwo (max-1,n))
+// printfn "- (max-1,1): %A" (hasPropertyTwo (max-1,1))
+// printfn "- (max,max-1): %A" (hasPropertyTwo (max, max-1))
+// printfn "- (max,n): %A" (hasPropertyTwo (max,n))
+// printfn "- (max,1): %A" (hasPropertyTwo (max,1)) 
 
 printfn "Iterative Property 1:"
 printfn "- (0,0): %A" (hasPropertyOneNoRec (0, 0))
 printfn "- (1,1): %A" (hasPropertyOneNoRec (1, 1))
-printfn "- (max,0): %A" (hasPropertyOneNoRec (max, 0))
-printfn "- (max,max): %A" (hasPropertyOneNoRec (max, max))
+// printfn "- (max,0): %A" (hasPropertyOneNoRec (max, 0))
+// printfn "- (max,max): %A" (hasPropertyOneNoRec (max, max))
 printfn "- (n,n): %A" (hasPropertyOneNoRec (n, n))
 printfn "- (n,0): %A" (hasPropertyOneNoRec (n, 0))
 
@@ -57,11 +60,14 @@ printfn "Iterative Property 2:"
 printfn "- (2,1): %A" (hasPropertyTwoNoRec (2,1))
 printfn "- (n,n-1): %A" (hasPropertyTwoNoRec (n,n-1))
 printfn "- (n,n/2): %A" (hasPropertyTwoNoRec (n,n/2))
-printfn "- (max-1,max-2): %A" (hasPropertyTwoNoRec (max - 1, max-2))
-printfn "- (max-1,n): %A" (hasPropertyTwoNoRec (max-1,n))
-printfn "- (max-1,1): %A" (hasPropertyTwoNoRec (max-1,1))
-printfn "- (max,max-1): %A" (hasPropertyTwoNoRec (max, max-1))
-printfn "- (max,n): %A" (hasPropertyTwoNoRec (max,n))
-printfn "- (max,1): %A" (hasPropertyTwoNoRec (max,1)) 
+// printfn "- (max-1,max-2): %A" (hasPropertyTwoNoRec (max - 1, max-2))
+// printfn "- (max-1,n): %A" (hasPropertyTwoNoRec (max-1,n))
+// printfn "- (max-1,1): %A" (hasPropertyTwoNoRec (max-1,1))
+// printfn "- (max,max-1): %A" (hasPropertyTwoNoRec (max, max-1))
+// printfn "- (max,n): %A" (hasPropertyTwoNoRec (max,n))
+// printfn "- (max,1): %A" (hasPropertyTwoNoRec (max,1)) 
 
+
+printfn "YEEEEEEEEET\n\n\n\n"
+pascalNoRec (34,1)
 

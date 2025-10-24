5e296fa68c7a1d2c941ffe55b9dc6949fc820c47
Ib-Loewe <ib.scherer@hotmail.com>
2025-10-24 13:31:38 +0200

    unstable version of pascal recursive

--- src/Pascal/Pascal.fs
+++ src/Pascal/Pascal.fs
@@ -4,29 +4,53 @@ module Pascal
 // pascal: int * int -> int
 let rec pascal (n: int, k: int) : int = 
     // check for natural numbers and than n >= k
-    if k <= 0 && n >= k then
-        match k with 
-        | 0 -> 1
-        | k when k = n -> 1
+    if k <= 0 && n <= 0 && n >= k then
+        match n, k with 
+        | _, 0 -> 1
+        | n, k when n = k -> 1
         | _ -> pascal (n - 1, k - 1) + pascal (n - 1, k)
     else
         //printf "pascal must be called with two natural numbers, n and k with n >= k"
         0
 
-// factorial helper funcion
-let factorial n = [1..n] |> List.fold (*) 1
-
-// Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k
-// pascalNoRec: int * int -> int
 let pascalNoRec (n: int, k: int) : int =
     // check for natual numberso and than n >= k
     if n < k || k < 0 then
         //printf "pascalNoRec must be called with two natural numbers, n and k with n >= k"
         0
     // check for edges
-    else if k = 0 || n = k then
+    else if n = k || k = 0 then
         1
-    // iterative calculation of placement in Pascal's triangle, with two mutable arrays moving down the triangle each iteration
+    else
+        // create 2d array (matrix) to store values, initialized to 1
+        let matrix : int array array = Array.zeroCreate (n + 1)
+        for i = 0 to n do
+            matrix.[i] <- Array.create (i + 1) 1
+        // fill in the matrix using pascal's rule
+        for i = 2 to n do
+            for j = 1 to i - 1 do
+                matrix.[i].[j] <- matrix.[i - 1].[j - 1] + matrix.[i - 1].[j]
+        // return matrix value n choose k
+        matrix.[n].[k]
+
+
+// factorial helper function
+let rec factorial n = 
+    match n with
+    | 0 | 1 -> 1
+    | _ -> factorial (n - 1) * n
+
+// Takes a pair of natural numbers n and k, and returns the corresponding binomial coefficient for n choose k.
+// Uses factorail helper function, so can overflowwith number that are too large
+// pascalFactCalc: int * int -> int
+let pascalFactCalc (n: int, k: int) : int =
+    // check for natual numberso and than n >= k
+    if n < k || k < 0 then
+        //printf "pascalNoRec must be called with two natural numbers, n and k with n >= k"
+        0
+    // check for edges
+    else if n = k || k = 0 then
+        1
     else
         factorial n / (factorial k * factorial (n-k))
 

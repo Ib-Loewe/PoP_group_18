1770afc25efcf8dbf1780a8fc4043170126a1731
Løwe <ib.scherer@hotmail.com>
2025-10-24 15:29:26 +0200

    finihsed myList

--- src/MyList/MyList.fs
+++ src/MyList/MyList.fs
@@ -7,18 +7,32 @@ type 'a MyList =
 
 //Takes an integer n and a MyList, and returns a MyList of the first n elements in the original list.
 let rec take (n: int) (lst: 'a MyList) : 'a MyList = 
-    Empty
+    match lst with
+    | Empty -> Empty
+    | Something (head, tail) -> 
+        if n <= 0 then
+            Empty
+        else
+            Something (head, take (n - 1) tail)
 
 
 //Takes an integer n and a MyList, and returns a MyList without the first n elements of the original list.
 let rec drop (n: int) (lst : 'a MyList) : 'a MyList =
-    Empty
+    match lst with
+    | Empty -> Empty
+    | Something (head, tail) ->
+        if n <= 0  then lst
+        else drop (n - 1) tail
 
 //Takes a MyList and returns the number of elements in the list.
 let rec length (lst: 'a MyList) : int = 
-    0
+    match lst with
+    | Empty -> 0
+    | Something (head, tail) -> 1 + length tail
 
 
 //Takes a function, and a MyList and returns a MyList with the function applied to each element.
 let rec map (f: 'a -> 'b) (lst: 'a MyList) : 'b MyList = 
-    Empty
+    match lst with
+    | Empty -> Empty
+    | Something (head, tail) -> Something (f head, map f tail)
--- src/MyList/MyListExamples.fsx
+++ src/MyList/MyListExamples.fsx
@@ -1,1 +1,44 @@
-printfn "Hello World"
+#load "MyList.fs"
+
+open MyList
+
+printfn "Running MyList examples..."
+let ex1 : int MyList =  Something (1, Something (2, Something (3, Something (4, Something (5, Empty)))))
+let ex2 : string MyList = Something ("a", Something ("b", Something ("c", Empty)))
+
+// take
+let ex1Take3 = take 3 ex1 // expected Something (1, Something (2, Something (3, Empty)))
+let ex2Take2 = take 2 ex2 // expected Something ("a", Something ("b", Empty))
+
+// drop
+let ex1Drop2 = drop 2 ex1 // expected Something (3, Something (4, Something (5, Empty)))    
+let ex2Drop1 = drop 1 ex2 // expected Something ("b", Something ("c", Empty))
+
+// Length
+let ex1Length = length ex1 // expected 5
+let ex2Length = length ex2 // expected 3
+let exEmptyLength = length Empty // expected 0
+
+// Map
+let ex1Mappedx2 = map (fun x -> x * 2) ex1 // expected Something (2, Something (4, Something (6, Something (8, Something (10, Empty)))))
+
+printfn "Examples executed."
+printfn "Example lists:"
+printfn "Example list 1: int MyList = %A" ex1
+printfn "Example list 2: string MyList = %A" ex2
+
+printfn "\nTake examples:"
+printfn "Example list 1: Take 3: = %A" ex1Take3
+printfn "Example list 2: Take 2 = %A" ex2Take2
+
+printfn "\nDrop examples:"
+printfn "Example list 1: Drop 2 = %A" ex1Drop2
+printfn "Example list 2: Drop 3 = %A" ex2Drop1
+
+printfn "\nLength examples:"
+printfn "Example list 1 Length = %A" ex1Length
+printfn "Example list 2 Length = %A" ex2Length
+printfn "Example empty list length = %A" exEmptyLength
+
+printfn "\nmap example:"
+printfn "Example list 1 mapped x2: %A" ex1Mappedx2
